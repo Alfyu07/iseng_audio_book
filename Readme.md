46203195a3402358b243a72593fc8d83ba1b8ffa
Alfyu07 <w.alfa433@gmail.com>
2021-03-02 15:07:34 +0800

    add readme.md

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Iseng Audio Books
+
+Iseng audio books adalah pdf books audio reader yang dibuat menggunakan [pyttsx3](https://pypi.org/project/pyttsx3/) dan [pyPDF2](https://pypi.org/project/PyPDF2/)
+
+## Requirement
+- python
+- pyttsx3
+- pyPDF2
+
+Instalasi pyttsx3 : 
+```bash
+pip install pyttsx3
+```
+Instalasi pyPDF2
+```bash
+pip install pyPDF2
+```
+
+## Usage
+Penggunaan cukup dengan menjalankan file python 'iseng_audio_book.py' melalui command :
+```bash
+python3 iseng_audio_book.py #python3
+#or
+python iseng_audio_book.py #python2
+```
--- iseng_audio_book.py
+++ iseng_audio_book.py
@@ -1,18 +1,44 @@
 import pyttsx3
 import PyPDF2
 
+#buka pdf dan baca dengan binary mode
 book = open('Buku Gratis - Mereka yang Merugi - Muhammad Abduh Tuasikal.pdf', 'rb')
 
+#pakai PyPDF2 untuk baca buku
 pdfReader = PyPDF2.PdfFileReader(book)
+
+#lihat banyak pages
 pages = pdfReader.numPages
 
+#inisialisasi engine speaker dari pytssx3
 speaker = pyttsx3.init()
+#ambil property voices untuk melihat mode dan bahasa 
 voices = speaker.getProperty('voices')
+
+"""
+property engine tssx bisa di atur dengan parameter2 dibawah ini
+for index, voice in enumerate(voices):
+    print("====" + str(index) + "====")
+    print("Voice: %s" % voice.name)
+    print(" - ID: %s" % voice.id)
+    print(" - Languages: %s" % voice.languages)
+    print(" - Gender: %s" % voice.gender)
+    print(" - Age: %s" % voice.age)
+    print("\n")
+"""
+
+#merubah voice yang digunakan ke bahasa indonesia
 speaker.setProperty('voice', voices[34].id)
 
-for num in range(12, pages):
+#definisiin variable start sebagai halaman awal
+start= 12
+
+for num in range(start, pages):
   page = pdfReader.getPage(num)
   text = page.extractText()
   print(text)
   speaker.say(text)
   speaker.runAndWait()
+  
+  
+#resource : https://betterprogramming.pub/an-introduction-to-pyttsx3-a-text-to-speech-converter-for-python-4a7e1ce825c3#:~:text=For%20Windows%20user%2C%20head%20over,see%20the%20following%20user%20interface.&text=Apart%20from%20the%20default%20language,to%20add%20a%20preferred%20language.
